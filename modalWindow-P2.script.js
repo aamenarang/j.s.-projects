3898fc3fd380c87b1f2fd5a56632a4930298ec68
AAMENA RANGWALA <72145473+aamenarang@users.noreply.github.com>
2022-02-25 22:53:38 +0530

    Add files via upload

---
+++ modalWindow-P2.script.js
@@ -0,0 +1,30 @@
+'use strict';
+const modal = document.querySelector('.modal');
+const overLay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+
+
+const openModal =  function() {
+    modal.classList.remove('hidden'); // No Need of DOT notation in classList
+    overLay.classList.remove('hidden');
+}
+
+const closeModal = function() {
+    modal.classList.add('hidden'); 
+    overLay.classList.add('hidden');
+}
+
+for(let i = 0; i < btnOpenModal.length; i++)
+btnOpenModal[i].addEventListener('click', openModal);
+btnCloseModal.addEventListener('click', closeModal);
+overLay.addEventListener('click', closeModal); // we dont call the function here, we just declare/define it in this case.
+
+document.addEventListener('keydown', function(e) {
+//console.log(e.key);
+
+if (e.key === 'Escape' && !modal.classList.contains('hidden')) 
+    {
+        closeModal();
+    }
+});
